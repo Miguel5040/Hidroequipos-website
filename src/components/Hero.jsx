0d066ee64ff073d26dd1fb7c374208ccaaf686dc
Miguel Alberto <miguelalberto@Miguels-MacBook-Air.local>
2024-12-11 11:20:22 -0700

    Little changes on navbar and hero sections

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,20 @@
 
 const Hero = () => {
   return (
-    <main className='flex relative justify-center items-center overflow-hidden'>
+    <main className='flex justify-center items-center w-screen h-screen brightness-90 bg-fixed bg-cover bg-[url("../src/assets/img/pool-hero-2.jpg")] bg-blend-darken'>
 
-      <div className='-z-10 brightness-90 h-screen lg:h-auto'>
-        <img className="object-cover h-full" src="../src/assets/img/pool-hero-2.jpg" alt="" />
-      </div>
-
-      <div className='absolute top-auto bottom-auto left-auto right-auto flex flex-col gap-6 items-center'>
+      <div className='flex flex-col gap-4 items-center'>
 
         <header className="flex flex-col items-center">
           <h1 className="font-bold md:text-5xl sm:text-4xl xs:text-3xl text-2xl text-white text-center">BUILT AND CARED FOR</h1>
-          <h1 className="font-bold md:text-5xl sm:text-4xl xs:text-3xl text-2xl text-white text-center mb-2">YOUR DREAM POOL PERFECTLY</h1>
+          <h1 className="font-bold md:text-5xl sm:text-4xl xs:text-3xl text-2xl text-white text-center">YOUR DREAM POOL PERFECTLY</h1>
         </header>
 
-        <div className="text-white text-center md:text-lg text-base text-balance bg-blue-400 px-4 py-1 rounded-full bg-opacity-70">Expert pool construction, maintenance, and care products in one place</div>
+        <div className="text-white text-center md:text-lg text-base text-balance px-4 py-1 rounded-full bg-opacity-70 mb-4">Expert pool construction, maintenance, and care products in one place</div>
+
+        <span>
+          <a className="text-white px-9 py-2 bg-cyan-700 rounded-lg" href="#">Learn more</a>
+        </span>
 
       </div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 
 const Navbar = () => {
   return (
-    <header className="py-3 px-10 flex justify-between items-center absolute right-0 left-0 z-10">
+    <header id="navbar" className="py-3 pb-20 px-10 flex justify-between items-center absolute right-0 left-0 z-10 backdrop-brightness-90 backdrop-blur-[2px]">
 
       <div className="flex flex-grow basis-0 z-20">
         <img className=" w-28" src="../src/assets/img/image-logo.png" alt="hidroequipos logo" />
@@ -16,8 +16,9 @@ const Navbar = () => {
         </ul>
       </nav>
 
-      <div className="lg:flex hidden flex-grow basis-0 justify-end">
-        <a className="text-white bg-[#8BC34A] px-6 py-1 rounded-full" href="#">Contact Us</a>
+      <div className="lg:flex lg-flex-col items-center gap-2 hidden flex-grow basis-0 justify-end">
+        <span className="text-white">🇺🇸 English</span>
+        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="white" className="icon icon-tabler icons-tabler-filled icon-tabler-triangle-inverted"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M20.118 3h-16.225a2.914 2.914 0 0 0 -2.503 4.371l8.116 13.549a2.917 2.917 0 0 0 4.987 .005l8.11 -13.539a2.914 2.914 0 0 0 -2.486 -4.386z" /></svg>
       </div>
 
       <div className="lg:hidden flex flex-grow basis-0 justify-end cursor-pointer">
