c70710d46637e158b11e8cfe6bb24b5a8869e91c
Miguel Alberto <miguelalberto@Miguels-MacBook-Air.local>
2025-01-09 09:51:36 -0700

    Services section responsive added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
 
       <div className='flex flex-col gap-4 items-center'>
 
-        <header className="flex flex-col items-center gap-2 mx-2">
+        <header className="flex flex-col items-center mx-2 leading-10">
           <h1 className="font-bold sm:text-5xl xs:text-4xl text-3xl text-white text-center text-balance">BUILT AND CARED FOR</h1>
           <h1 className="font-bold sm:text-5xl xs:text-4xl text-3xl text-white text-center">YOUR DREAM POOL PERFECTLY</h1>
         </header>
@@ -18,7 +18,7 @@ const Hero = () => {
 
       </div>
 
-      <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-compact-down absolute bottom-3"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 11l8 3l8 -3" /></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-compact-down absolute bottom-3"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 11l8 3l8 -3" /></svg>
 
     </main>
   )
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,34 +1,35 @@
 
 const Services = () => {
   return (
-    <section className="max-w-screen-xl mx-auto">
+    <section className="max-w-screen-xl mx-auto px-3">
 
       <header className="flex flex-col gap-3 items-center mt-14 mb-10">
-        <h2 className="text-3xl">Top Rated Swimming Pool Company in Los Cabos</h2>
-        <p className="text-center">Lorem ipsum dolor sit amet consectetur adipiscing elit litora, sociis egestas nec bibendum mus nibh ultrices sapien, potenti ac imperdiet fames nam dis nullam.</p>
+        <h2 className="text-3xl text-center text-pretty">Top Rated Swimming Pool Company in Los Cabos</h2>
+        <p className="text-center text-pretty">Lorem ipsum dolor sit amet consectetur adipiscing elit litora, sociis egestas nec bibendum mus nibh ultrices sapien, potenti ac imperdiet fames nam dis nullam.</p>
       </header>
 
-      <main className="flex gap-4 mx-10 items-center justify-center">
-        <div className="w-[29.16666%]">
-          <img className="rounded-lg aspect-[10/11] object-cover mb-2" src="../src/assets/img/pool-filtration.jpeg" alt="Pool filtration system" />
-          <div>
-            <h3 className="text-xl font-medium mb-1">Replacement and repair</h3>
-            <p className="text-sm">We make sure that everything works perfectly</p>
-          </div>
-        </div>
+      <main className="flex flex-row lg:flex-nowrap flex-wrap gap-4 lg:mx-10 lg:items-center lg:justify-center">
 
-        <div className="w-[41.16666%] flex justify-center ">
-          <div className="relative">
-            <img className="rounded-lg aspect-[4/5] max-h-[550px] object-cover" src="../src/assets/img/pool-8.jpg" alt="Pool presentation" />
-            <div className="absolute top-5 left-0 rounded-ee-3xl bg-white flex flex-col pl-4 pr-2 py-2 max-w-[90%]">
+        <div className="lg:w-[41.16666%] w-full lg:h-auto h-[280px] flex justify-center">
+          <div className="relative w-full overflow-hidden">
+            <img className="rounded-lg lg:aspect-[4/5] lg:max-h-[550px] h-full w-full object-cover object-center" src="../src/assets/img/pool-8.jpg" alt="Pool presentation" />
+            <div className="absolute top-5 left-0 rounded-ee-3xl bg-white flex flex-col pl-4 lg:pr-2 pr-8 py-3 max-w-[90%]">
               <span className="font-medium">Swimming pool Construction</span>
               <span className="font-light text-sm">We design, build and maintain swimming pools in all of Los Cabos</span>
             </div>
           </div>
         </div>
 
-        <div className="w-[29.16666%]">
-          <img className="rounded-lg aspect-[10/11] object-cover object-left mb-2" src="../src/assets/img/probador-sal-img.png" alt="Probador de sal" />
+        <div className="lg:w-[29.16666%] w-[calc(60%-8px)] lg:-order-1">
+          <img className="rounded-lg lg:aspect-[10/11] lg:object-cover mb-2 w-full object-center" src="../src/assets/img/pool-filtration.jpeg" alt="Pool filtration system" />
+          <div>
+            <h3 className="text-xl font-medium mb-1">Replacement and repair</h3>
+            <p className="text-sm">We make sure that everything works perfectly</p>
+          </div>
+        </div>
+
+        <div className="lg:w-[29.16666%] w-[calc(40%-8px)] flex flex-col justify-between">
+          <img className="rounded-lg lg:aspect-[10/11] object-cover object-left mb-2 flex-grow" src="../src/assets/img/probador-sal-img.png" alt="Probador de sal" />
           <div>
             <h3 className="text-xl font-medium mb-1">Swimming pool maintenance</h3>
             <p className="text-sm">We do our best home and commercial pool maintenance</p>
