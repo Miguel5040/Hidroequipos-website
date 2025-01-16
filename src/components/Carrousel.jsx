c95520732542e74856c990675669c7629313b91a
Miguel Alberto <miguelalberto@Miguels-MacBook-Air.local>
2025-01-16 15:37:48 -0700

    About section added

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -3,26 +3,26 @@ import './Carrousel.css'
 const Carrousel = () => {
 
   return (
-    <section className="logo lg:px-8 px-3 mt-6">
+    <section className="logo lg:px-8 px-3 my-6">
       <div className='carrousel'>
         <div className="logo-slide">
           <img src="../src/assets/img/Pentair.png" />
           <img src="../src/assets/img/Hayward.png" />
-          <img src="../src/assets/img/CMP.png" />
-          <img src="../src/assets/img/Jandy.png" />
-          <img src="../src/assets/img/Innowater.png" />
-          <img src="../src/assets/img/Fluidra.png" />
           <img src="../src/assets/img/Raypack.png" />
+          <img src="../src/assets/img/Jandy.png" />
+          <img src="../src/assets/img/Fluidra.png" />
+          <img src="../src/assets/img/Innowater.png" />
+          <img src="../src/assets/img/CMPTEST.png" />
         </div>
 
         <div className="logo-slide">
           <img src="../src/assets/img/Pentair.png" />
           <img src="../src/assets/img/Hayward.png" />
-          <img src="../src/assets/img/CMP.png" />
-          <img src="../src/assets/img/Jandy.png" />
-          <img src="../src/assets/img/Innowater.png" />
-          <img src="../src/assets/img/Fluidra.png" />
           <img src="../src/assets/img/Raypack.png" />
+          <img src="../src/assets/img/Jandy.png" />
+          <img src="../src/assets/img/Fluidra.png" />
+          <img src="../src/assets/img/Innowater.png" />
+          <img src="../src/assets/img/CMPTEST.png" />
         </div>
 
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero.jsx'
 import Services from '../components/Services.jsx'
 import Design from '../components/Design.jsx'
 import Carrousel from '../components/Carrousel.jsx'
+import About from '../components/About.jsx'
 import Sidebar from '../components/Sidebar.jsx'
 import Hamburger from '../components/Hamburger.jsx'
 import { SidebarProvider } from '../context/SidebarContext.jsx'
@@ -19,6 +20,7 @@ const Home = () => {
       <Services />
       <Design />
       <Carrousel />
+      <About />
     </main >
   )
 }
